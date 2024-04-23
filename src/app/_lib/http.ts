11c52066c6d7276ff4afa984d2a9886f19e72a25
ngayMaivanden <datlai304@gmail.com>
2024-04-23 21:44:31 +0700

    them ham goi api again

--- src/app/_lib/http.ts
+++ src/app/_lib/http.ts
@@ -154,6 +154,7 @@ export const resquest = async <Response>(method: Method, url: string, options?: 
 					const call_again = await fetch(fullUrl, {
 						method,
 						body,
+						credentials: "include",
 
 						headers: {
 							...baseHeader,
