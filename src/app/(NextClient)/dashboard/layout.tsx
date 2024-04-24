c5c00da4da2b98a2a3ef92a0cc82e8f588b0ba8d
ngayMaivanden <datlai304@gmail.com>
2024-04-24 14:38:24 +0700

    Cap nhap page dashboard

--- src/app/(NextClient)/dashboard/layout.tsx
+++ src/app/(NextClient)/dashboard/layout.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import SidebarContextProvider from "./SidebarContext";
 import UserService from "@/app/_services/user.service";
 import { UserType } from "@/app/_schema/user/user.type";
-type TProps = {
-	Children: React.ComponentType<{ user: UserType | null }>;
-};
-const DashBoardLayout = async (props: TProps) => {
-	const { Children } = props;
 
+const DashBoardLayout = async ({ Children }: { Children: React.ComponentType<{ user: UserType | null }> }) => {
 	let user = null;
 	try {
 		const res = await UserService.me();
